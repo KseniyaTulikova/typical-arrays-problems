8ec08889b366d2da6f5c9f39d775e4ff9e36c0f5
Kseniya Tulikava <ksenia77383@gmail.com>
2020-03-15 16:43:43 +0300

    feat: add solutions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array = []) {
+  let minElem = array.length != 0 ? array[0] : 0;
+  array.forEach(element => {
+    if (element < minElem) {
+      minElem = element;
+    }
+  });
+
+  return minElem;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array = []) {
+  let maxElem = array.length != 0 ? array[0] : 0;
+
+  array.forEach(element => {
+    if (element > maxElem) {
+      maxElem = element;
+    }
+  });
+
+  return maxElem;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array = []) {
+  let sumOfAllElements = 0;
+
+  array.forEach(elem => sumOfAllElements += elem);
+
+  return array.length != 0 ? sumOfAllElements / array.length : 0;
 }
